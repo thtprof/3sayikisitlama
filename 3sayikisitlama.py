4176a21263185b0a178dbbf64fe21e0cfdbc47ae
PROFESÖR THT <77558299+thtprof@users.noreply.github.com>
2021-05-24 21:24:52 +0300

    Create 3sayikisitlama.py

---
+++ 3sayikisitlama.py
@@ -0,0 +1,41 @@
+"""
+Not hesaplama
+
+
+
+
+"""
+
+a=int(input("1. Sayiyi giriniz: "))
+b=int(input("2. Sayiyi giriniz: "))
+c=int(input("3. Sayiyi giriniz: "))
+
+if a>b>c:
+  print(a,">",b,">",c)
+elif a>c>b:
+  print(a,">",c,">",b)
+elif b>a>c:
+  print(b,">",a,">",c)
+elif b>c>a:
+  print(b,">",c,">",a)
+elif c>a>b:
+  print(c,">",a,">",b)
+elif c>b>a:
+  print(c,">",b,">",a)
+elif a==b>c:
+  print(a,"==",b,">",c)
+elif a>c==b:
+  print(a,">",c,"==",b)
+elif b==a>c:
+  print(b,"==",a,">",c)
+elif b>c==a:
+  print(b,">",c,"==",a)
+elif b==a>b:
+  print(c,"==",a,">",b)
+elif c>b==a:
+  print(c,">",b,"==",a)
+input()
+    
+    
+    
+    
